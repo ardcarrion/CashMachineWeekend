cf7402a81bc1869bcd71565923324471d9cdbbda
benrybinski <benrybin@gmail.com>
2019-06-09 11:49:15 -0400

    Update Loan.java

--- src/main/java/rocks/zipcode/atm/bank/Loan.java
+++ src/main/java/rocks/zipcode/atm/bank/Loan.java
@@ -1,8 +1,21 @@
 package rocks.zipcode.atm.bank;
 
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
+
 public class Loan {
 
     public static void main(){
+        TextInputDialog loanAmt = new TextInputDialog("");
+        TextInputDialog creditScore = new TextInputDialog("");
+        loanAmt.setTitle("Loan Application");
+        loanAmt.setHeaderText("All information must be filled in!");
+        loanAmt.setContentText("Please enter loan amount:");
+
+
+        Optional<String> loanAmtTemp = loanAmt.showAndWait();
+        Optional<String> creditScoreTemp = creditScore.showAndWait();
 
 
 
