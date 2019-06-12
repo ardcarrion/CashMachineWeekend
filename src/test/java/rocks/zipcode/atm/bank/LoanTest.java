96ab2f7f6330ed7841f3c26de9d39ca65414ff35
benrybinski <benrybin@gmail.com>
2019-06-12 08:35:06 -0400

    test

---
+++ src/test/java/rocks/zipcode/atm/bank/LoanTest.java
@@ -0,0 +1,47 @@
+package rocks.zipcode.atm.bank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class LoanTest {
+
+    @Test
+    public void getLnAmount() {
+
+        
+    }
+
+    @Test
+    public void setLnAmount() {
+    }
+
+    @Test
+    public void getCreditScore() {
+    }
+
+    @Test
+    public void setCreditScore() {
+    }
+
+    @Test
+    public void loanYes() {
+    }
+
+    @Test
+    public void setIntRate() {
+    }
+
+    @Test
+    public void getIntRate() {
+    }
+
+    @Test
+    public void setLoanStatus() {
+    }
+
+    @Test
+    public void getLoanStatus() {
+    }
+}
